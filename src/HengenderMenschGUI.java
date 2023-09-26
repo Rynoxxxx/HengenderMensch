70cb6ef9ddde393d8df209759175f92e9984efb5
samuel.stroot <samuel.stroot@gmail.com>
2023-09-26 13:30:45 +0200

    tt

--- src/HengenderMenschGUI.java
+++ src/HengenderMenschGUI.java
@@ -1,12 +1,51 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class HengenderMenschGUI {
 
 
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JTextField strichFeld;
+    private JTextField buchstabenEingabe;
+    private JLabel titel;
+    private JTextField wortEingabe;
+    private JButton stricheGenerierenButton;
+
+    String wort;
+    String [] buchstabe;
+    public HengenderMenschGUI() {
+
+        stricheGenerierenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                wort = wortEingabe.getText();
+                for (int i=0; i<wort.length();i++){
+                    strichFeld.setText(strichFeld.getText()+"- ");
+                }
+                wortEingabe.setVisible(false);
+                stricheGenerierenButton.setVisible(false);
+            }
+        });
+
+        buchstabenEingabe.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                strichFeld.setText("");
+                buchstabe= new String [wort.length()];
+                for(int i=0; i<buchstabe.length; i++){
+
+                    buchstabe[i]="-";
+                    strichFeld.setText(strichFeld.getText()+buchstabe[i]+" ");
+                }
+
+
+
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("HengenderMenschGUI");
@@ -14,6 +53,7 @@ public class HengenderMenschGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
 
     }
 }
